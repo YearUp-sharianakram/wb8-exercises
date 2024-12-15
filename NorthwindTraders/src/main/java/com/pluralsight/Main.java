80ff7b54be54b76a439cfaa205dd2ba6e472d7a0
sharianakram <sharianakram0503@gmail.com>
2024-12-15 13:42:23 -0500

    northwind traders 1

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -2,32 +2,43 @@ package com.pluralsight;
 
 import java.sql.*;
 
+
 public class Main {
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        // load the MySQL Driver
+        String username = "root";
+        String password = "yearup";
+
         Class.forName("com.mysql.cj.jdbc.Driver");
-// 1. open a connection to the database
-// use the database URL to point to the correct database
+
+        // 1. open a connection to the database
+        // use the database URL to point to the correct database
         Connection connection;
         connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/northwind",
-                "root",
-                "yearup");
-// create statement
-// the statement is tied to the open connection
+                username,
+                password);
+
+
+        // create statement
+        // the statement is tied to the open connection
         Statement statement = connection.createStatement();
-// define your query
-        String query = "SELECT * FROM products ";
-// 2. Execute your query
+
+        // define your query
+        String query = "SELECT * FROM products";
+
+        // 2. Execute your query
         ResultSet results = statement.executeQuery(query);
-// process the results
-        while (results.next()) {
-            String productNames = results.getString("ProductName");
-            System.out.println(productNames);
+
+        // process the results
+        while(results.next()){
+            String productName = results.getString("ProductName");
+            System.out.println(productName);
         }
-// 3. Close the connection
+
+        // THIS WILL CLOSE ALL CONNECTIONS
+        results.close();
+        statement.close();
         connection.close();
-
-
     }
 }
